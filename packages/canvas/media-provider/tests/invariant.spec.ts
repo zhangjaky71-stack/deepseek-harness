48886fd5ab3a2ccbf4320afc8c7c00a8277b82a1
zhangjaky71-stack <zhangjaky71@gmail.com>
2026-08-22 21:53:48 +0800

    test(canvas): use rc.2 Cordis teardown

--- packages/canvas/media-provider/tests/invariant.spec.ts
+++ packages/canvas/media-provider/tests/invariant.spec.ts
@@ -9,7 +9,7 @@ import { model, provider } from './model-fixture.ts'
 
 const contexts: Context[] = []
 afterEach(async () => {
-  while (contexts.length > 0) await contexts.pop()!.dispose()
+  while (contexts.length > 0) await contexts.pop()!.fiber.dispose()
 })
 
 const runtime: MediaProvider = {
